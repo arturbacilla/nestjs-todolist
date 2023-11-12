16363cdc5848a754053dd51f685d398fffcf00b2
arturbacilla <artur.bacilla1@gmail.com>
2023-11-12 15:54:00 -0300

    feat(backend, auth): Added auth module and guard as the nest documentation

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -2,14 +2,28 @@ import { Module } from '@nestjs/common';
 import { TaskController } from './task/task.controller';
 import { TaskService } from './task/task.service';
 import { PrismaModule } from 'nestjs-prisma';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     PrismaModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [TaskController],
-  providers: [TaskService],
+  providers: [
+    TaskService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
